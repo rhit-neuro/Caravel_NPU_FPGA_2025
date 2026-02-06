49089d88c1e9cbd9fd61a9d5c5a4dddb1828d09a
Hannah Zawaideh <zawaidhm@rose-hulman.edu>
2026-02-06 14:34:48 -0500

    testing h accumulator

--- CARAVEL/CARAVEL.srcs/sources_1/new/SM_h_accumulator.v
+++ CARAVEL/CARAVEL.srcs/sources_1/new/SM_h_accumulator.v
@@ -21,16 +21,16 @@
 
 
 module SM_h_accumulator(
+    input reset,
+    input enable_h,
     input [31:0] h0,
     input [31:0] h_tn1,
     input [31:0] tau_rise_inverse,
     input actionPotential_tn1,
-    input [1:0] synapseID,
     input [31:0] dt,
-    output [31:0] h_t,
-    output exception
-//    output [1:0] synapseID_forward,
-//    output [31:0] dt_forward
+    output reg [31:0] h_t,
+    output reg exception,
+    output reg done_h
     );
     
     
@@ -39,13 +39,10 @@ module SM_h_accumulator(
     wire underF_MAC1,underF_MAC2;
     
     
-    wire [31:0] result_MAC1;
+    wire [31:0] result_MAC1, result_MAC2;
 
     wire [31:0] b_MAC1;
     assign b_MAC1 = actionPotential_tn1 ? h0 : 0; //if (event detected in pre-synaptic cell): add h0 , else: add 0
-
-    //assign synapseID_forward = synapseID;
-//    assign dt_forward = dt;
 
     
     LUT_MAC_Module #(.DataWidth(32)) MAC1(
@@ -65,11 +62,23 @@ module SM_h_accumulator(
         .Exception(except_MAC2),
         .Overflow(overF_MAC2),
         .Underflow(underF_MAC2),
-        .result(h_t)
+        .result(result_MAC2)
     );
 
-
-    assign exception = except_MAC1 | except_MAC2;
+    always @(reset or enable_h or result_MAC2)begin
+        if(reset)begin
+            done_h = 0;
+        end
+        else if(enable_h) begin
+            exception = except_MAC1 | except_MAC2;
+            h_t = result_MAC2;
+            done_h = 1;
+        end else begin
+            h_t = h_tn1;
+            done_h = 0;
+        end
+        
+     end 
         
         
 endmodule
